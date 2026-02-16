8c87f17fd8aae5c3bf833d4a94fa3d539d6b96c6
laurathomas-roy <laura.thomas-roy@mymail.champlain.edu>
2026-02-16 11:59:45 -0500

    secure ssh

---
+++ linux/rocky/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author laurathomas-roy
+#creates a new ssh user using the $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "CODE GOES HERE"
